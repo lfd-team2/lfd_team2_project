ef9f4fdd6608f46e475d7691cff78c1f084e0e29
fatih.baskin <fatih.baskin@tokeninc.com>
2024-01-06 18:27:10 +0300

    best submission using random dropping of majority class, min-max normalization and median imputing

--- try_fatih.py
+++ try_fatih.py
@@ -9,27 +9,30 @@ from imblearn.under_sampling import RandomUnderSampler
 
 data_frame = pd.read_csv('data/aps_failure_training_set.csv', na_values='na')
 
+# cols_to_drop = []
+# for col in data_frame.columns:
+#    if data_frame[col].isna().mean() > 0.70:
+#        cols_to_drop.append(col)
+
 training_data = data_frame.drop(columns=['id', 'class']).values
 training_class = data_frame['class'].values
 
-# Normalizing the data
-training_data, mean, std = nmr.zscore_normalize(
-    training_data, clipping=True, clip_min_percentile=2.5, clip_max_percentile=97.5)
-
-# Imputing the data
-training_data = imp.imputeMedian(training_data)
+# Dropping the columns with more than 70% missing values
 
 # Undersampling the data
-undersampler = RandomUnderSampler(random_state=0, sampling_strategy='majority')
+undersampler = RandomUnderSampler(
+    random_state=42, sampling_strategy='majority')
+
 training_data, training_class = undersampler.fit_resample(
     training_data, training_class)
 
-# Applying PCA
-training_data, eigenvectors, eigenvalues = pca(training_data, 70)
-pca_components = eigenvectors[:, 0:70]
+# Imputing the data
+training_data, min, minmax = nmr.minmax_normalize(
+    training_data)
+training_data = imp.imputeMedian(training_data)
 
 random_forest = RandomForestClassifier(
-    n_estimators=100, random_state=0, max_depth=10)
+    n_estimators=100, random_state=42)
 
 random_forest.fit(training_data, training_class)
 
@@ -37,17 +40,9 @@ random_forest.fit(training_data, training_class)
 test_frame = pd.read_csv('data/aps_failure_test_set.csv', na_values='na')
 test_data = test_frame.drop(columns=['id']).values
 
-# Normalizing the data
-# test_data, mean, std = nmr.zscore_normalize(test_data)
-test_data = nmr.clipping_normalize(test_data, 2.5, 97.5)
-test_data = nmr.applyZscoreNormalization(test_data, mean, std)
-
 # Imputing the data
+test_data = nmr.applyMinmaxNormalization(test_data, min, minmax)
 test_data = imp.imputeMedian(test_data)
-
-# Applying PCA
-test_data = np.dot(test_data, pca_components)
-
 
 # Predicting the test data
 test_class = random_forest.predict(test_data)
